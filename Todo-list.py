bd4153bd04fba361564e27f41a5a22c7237e6cdd
Muhammadghaniabid <muhammadghaniabid@gmail.com>
2026-03-28 07:56:03 -0800

    Add files via upload
    Daily Python learning and practice repository.

---
+++ Todo-list.py
@@ -0,0 +1,16 @@
+print("Enter a Todo")
+my_todo=input(
+)
+print(my_todo)
+
+message = 'Enter the ToDo:'
+my_todo=input(message)
+print(my_todo)
+
+prompt="Enter your Todo:"
+todo1=input(prompt)
+todo2=input(prompt)
+todo3=input(prompt)
+
+todos = [todo1,todo2,todo3]
+print(todos)
